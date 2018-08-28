5a1de40447cdbe8dfe4aca466db37604cc6aeb9d
JarlTuxen <jart@kea.dk>
2018-08-28 14:18:35 +0200

    Dat18C demo

---
+++ code/HelloDat18C.java
@@ -0,0 +1,11 @@
+public class HelloDat18C{
+   
+   public static void main(String[] args){
+      //Udskriv noget til skærmen
+      System.out.println("Hello \"Dat18C!\"");
+      System.out.println("Vær velkommen til 1. semester.");
+      /*
+      meget lang kommentar
+      */
+   }
+}
